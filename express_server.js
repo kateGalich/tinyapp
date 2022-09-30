651877fd3b3e4b18a304fccdd379e1a25a297466
Kate Galich <kate.galich@outlook.com>
2022-09-30 10:13:01 -0400

    Added Edit button to each object

--- express_server.js
+++ express_server.js
@@ -77,3 +77,11 @@ app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[id];
   res.redirect('/urls');
 });
+
+
+app.post("/urls/:id/edit", (req, res) => {
+  const id = req.params.id;
+  
+   urlDatabase[id] = req.body.longURL;
+  res.redirect('/urls');
+});
