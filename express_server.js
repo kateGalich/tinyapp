40dd70a2fbd838fd9fd3904fcbdcad9c7cb45413
Kate Galich <kate.galich@outlook.com>
2022-09-30 06:50:31 -0400

    Added a form element for each shortURL which uses a POST method

--- express_server.js
+++ express_server.js
@@ -70,3 +70,10 @@ app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[id];
   res.redirect(longURL);
 });
+
+
+app.post("/urls/:id/delete", (req, res) => {
+  const id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect('/urls');
+});
